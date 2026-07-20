1b5c6d6d9d88ffcf74ffb59da21ca02ed46b154f
Koga Togari <x25062xx@aitech.ac.jp>
2026-07-21 01:53:19 +0900

    HTML / CSS / js
    worksパージのほとんどが完成
    残り作業：作品の数を増やす
    　　　　　スマホ版を作る

--- js/works.js
+++ js/works.js
@@ -23,3 +23,19 @@ document.querySelector(".burger").addEventListener('click', () => {
         document.querySelector(".burger").classList.remove('burger-open');
     }
 });
+
+/*arrowをhover差し替え*/
+document.querySelectorAll(".hover-arrow").forEach((aicon) => {
+    const whiteSrc = aicon.querySelector('img').src;
+    //〇〇.replace(◻︎, △) -> 〇〇の画像名が入る(◻︎までを、△に変更)
+    const brownSrc = whiteSrc.replace('.svg', '_brown.svg');
+
+    aicon.addEventListener('mouseenter', () => {
+        aicon.querySelector('img').src = brownSrc;
+    });
+    aicon.addEventListener('mouseleave', () => {
+        aicon.querySelector('img').src = whiteSrc;
+    });
+});
+
+
